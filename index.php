40dc69742cad54c5af6eba362c8db5d709e96eb5
Les Ansley <les@ansleyfamily.com>
2015-06-02 23:28:39 +0100

    Move to responsive design

--- index.php
+++ index.php
@@ -1,14 +1,24 @@
-<?php
-        require_once 'header.php';
-        
-        echo "<br><span class="main">Welcome to the $appname,";
-        
-        if($loggedin)
-                echo " $username, you are logged in.";
-        else
-                echo " please <a href='register.php'>register</a> or <a href='login.php'>log in</a> to enter.";
-?>
-
-        </span><br><br>
-    </body>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Infection Registry</title>
+		<style href="css/main.css" rel="stylesheet" type="text/css"></style>
+		<meta name="viewport" content="width=device-width">
+		<meta charset="utf-8">
+	</head>
+	<body>
+		<div class="container">
+			<header class="row">
+				<div class="header-img"></div>
+				<div class="header-text"></div>	
+			</header>
+			<nav class="nav-bar">
+				<div></div>
+			</nav>
+			<main>
+				<div>
+				</div>
+			</main>
+		</div>
+	</body>
 </html>
